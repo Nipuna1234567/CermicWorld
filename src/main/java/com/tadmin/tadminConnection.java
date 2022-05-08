ff72523a9cc07259bb2e73aa2b8d674fb7cfdafb
SashinthaD <sashintha1999@gmail.com>
2022-05-08 13:11:17 +0530

    Transport Admin Login

--- src/main/java/com/tadmin/tadminConnection.java
+++ src/main/java/com/tadmin/tadminConnection.java
@@ -7,7 +7,7 @@ public class tadminConnection {
 	
 	private static String url="jdbc:mysql://localhost:3306/ceramicworld";
 	private static String user="root";
-	private static String password="Malshi1999";
+	private static String password="SHD123";
 	private static Connection con;
 
 	public static Connection getConnection() {
